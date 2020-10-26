f2b97eb82f615453f7cf5925ab79cd2e1ed20140
Sthephany Granados <lsgranados04@gmail.com>
2020-10-25 22:28:08 -0500

    docs add

---
+++ docs/precache-manifest.e6d4ed4a3aa02a147a443438f301b2b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09e1afcbc729391540441577cb70a8fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea76ca26303e7cba8ee2",
+    "url": "/static/css/main.63a346e2.chunk.css"
+  },
+  {
+    "revision": "4176c42ede5b2cef3754",
+    "url": "/static/js/2.b4b9e6aa.chunk.js"
+  },
+  {
+    "revision": "9cf9d893b5c51de0aa6dd9e07be64537",
+    "url": "/static/js/2.b4b9e6aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea76ca26303e7cba8ee2",
+    "url": "/static/js/main.abb54dc4.chunk.js"
+  },
+  {
+    "revision": "6bfc35ff2e0153f3a911",
+    "url": "/static/js/runtime-main.a3ae9f66.js"
+  },
+  {
+    "revision": "bbea47742842a4b63f8f6977c281328d",
+    "url": "/static/media/Fondo Burguer.bbea4774.png"
+  }
+]);
